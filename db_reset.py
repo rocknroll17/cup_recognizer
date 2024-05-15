c1557a8d6b0b9749b979c335a387547f4ac54957
rocknroll <gon1397@naver.com>
2024-05-15 23:56:25 +0900

    Recommit db_reset cause' it didn't added at last commit

--- db_reset.py
+++ db_reset.py
@@ -3,7 +3,6 @@ conn = sqlite3.connect('qr.db')
 cur = conn.cursor()
 cur.execute('SELECT * from qr')
 result = cur.fetchall()
-print(result)
 for i in range(len(result)):
     cur.execute('UPDATE qr SET name = NULL WHERE qr_code = ?', (result[i][0],))
 conn.commit()
