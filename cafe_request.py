6b3862155bada40e8f4ed712083f6f3fac102c16
dubae <128446180+dubae@users.noreply.github.com>
2024-05-19 03:19:46 +0900

    Update cafe_request.py

--- cafe_request.py
+++ cafe_request.py
@@ -1,7 +1,7 @@
 import requests
 import random
 # 사용자 이름을 포함한 데이터를 생성
-data = {"id": str(random.randint(1,9)), "name":"name"+str(random.randint(1,9)), "original_price": str(random.randint(1,9)*1000), "price": str(random.randint(1,9)*1000+300)}
+data = {"orderItems":[{"id": str(random.randint(1,9)), "name":"name"+str(random.randint(1,9)), "original_price": str(random.randint(1,9)*1000), "price": str(random.randint(1,9)*1000+300)}]}
 
 # POST 요청을 보내고 응답을 받음
 response = requests.post("http://localhost:4440/order_request", json=data)
